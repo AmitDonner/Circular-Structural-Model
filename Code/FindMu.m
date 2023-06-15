f5a9f4fe28274aa35430f831310b26ca239a4fc7
Amit Donner <101360875+AmitDonner@users.noreply.github.com>
2023-06-15 10:25:00 +0300

    Add files via upload

--- Code/FindMu.m
+++ Code/FindMu.m
@@ -4,7 +4,7 @@ function [mu,lambda]=FindMu(Sxx, Syy, Sxy)
     [V,D]=eig(Sigma,'vector');
     [~,ind]=min(D);
     %% Finding mu
-    mu_temp(1)=atan(V(2,ind)/V(1,ind));
+    mu_temp(1)=mod(atan(V(2,ind)/V(1,ind)) + 2*pi, 2*pi);
     mu_temp(2)=mod(mu_temp(1)+pi,pi*2);
     mu = sort(mu_temp);
     lambda = D;
